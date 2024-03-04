cb65b34e51e1e13ddec862ef1993e8670d672e0a
Ievia <148125183+Ievia@users.noreply.github.com>
2024-03-04 13:17:36 -0500

    basic template

---
+++ Loader/Loader.py
@@ -0,0 +1,48 @@
+import psycopg
+import json
+
+# assumes the data location
+with open('../open-data-0067cae166a56aa80b2ef18f61e16158d6a7359a/data/competitions.json', 'r') as file:
+    competitions = json.load(file)
+
+with psycopg.connect("dbname=comp3005finalproject user=postgres") as conn:
+    with conn.cursor() as cur:
+
+        # COMPETITIONS
+        cur.execute(open("competitions.sql", "r").read())
+        for row in competitions:
+            cur.execute("""
+                INSERT INTO competitions (competition_id, 
+                                          season_id, 
+                                          country_name, 
+                                          competition_name,
+                                          competition_gender,
+                                          competition_youth,
+                                          competition_international,
+                                          season_name,
+                                          match_updated,
+                                          match_updated_360,
+                                          match_available_360,
+                                          match_available)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            """, (row['competition_id'],
+                  row['season_id'],
+                  row['country_name'],
+                  row['competition_name'],
+                  row['competition_gender'],
+                  row['competition_youth'],
+                  row['competition_international'],
+                  row['season_name'],
+                  row['match_updated'],
+                  row['match_updated_360'],
+                  row['match_available_360'],
+                  row['match_available']))
+
+        cur.execute("SELECT * FROM competitions")
+        cur.fetchone()
+
+        for record in cur:
+            print(record)
+
+        # Make the changes to the database persistent
+        conn.commit()
---
+++ Loader/competitions.sql
@@ -0,0 +1,18 @@
+-- TODO
+
+-- COMPETITIONS
+create table competitions
+(
+    competition_id            integer,
+    season_id                 integer,
+    country_name              varchar,
+    competition_name          varchar,
+    competition_gender        varchar,
+    competition_youth         bool,
+    competition_international bool,
+    season_name               varchar,
+    match_updated             varchar,
+    match_updated_360         varchar,
+    match_available_360       varchar,
+    match_available           varchar
+);
